d7d1f5da635e80d87bbe932d8c1a5d1f03ae748a
Phil Fevry <pf@phlfvry.com>
2017-11-06 22:02:24 -0500

    Created model class. Added unit test methods.

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,42 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  This is the data model for the app.
+//
+//  Created by Phil on 11/6/17.
+//  Copyright © 2017 phlfvry. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+
+        // Name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // Rating must be between 0 and 5 inclusive
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        
+        // Initialize stored properties
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+    }
+    
+}
